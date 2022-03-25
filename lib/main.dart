ae0bc6ff2d205582b4aabf9e7f05ec154d31ee9d
tihon49 <a.tihonov@aerodisk.ru>
2022-03-25 17:59:46 +0300

    init

---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(MyApp());
+// }
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  int counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(primaryColor: Colors.deepOrangeAccent),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('ItPorgger App'),
+          centerTitle: true,
+        ),
+        body: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+          Column(
+            children: [
+              Text('$counter'),
+              TextButton(onPressed: () {}, child: Text('Hello'))
+            ],
+          ),
+          Column(
+            children: [
+              Text('Hello'),
+              TextButton(onPressed: () {}, child: Text('Hello'))
+            ],
+          ),
+        ]),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            counter++;
+            print(counter);
+          },
+          child: Text('Нажми'),
+        ),
+        //   (
+        //   child: Text('Push me'),
+        //   backgroundColor: Colors.deepOrangeAccent,
+        // ),
+      ),
+    );
+  }
+}
